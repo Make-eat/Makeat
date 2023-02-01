314f7fa56d638615bf83a228e5fc7250ac5bbb60
cookie0010 <sg.been2017@gmail.com>
2023-02-02 00:48:15 +0900

    백엔드 초기 세팅

---
+++ backend/src/main/java/pangyo/makeat/MakeatApplication.java
@@ -0,0 +1,13 @@
+package pangyo.makeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeatApplication.class, args);
+	}
+
+}
