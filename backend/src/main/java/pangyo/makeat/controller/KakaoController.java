6bbfc0ed05ad5b225947e6686f0676357f49653f
cookie0010 <sg.been2017@gmail.com>
2023-02-07 10:34:06 +0900

    feat: dao dto service 분리

--- backend/src/main/java/pangyo/makeat/controller/KakaoController.java
+++ backend/src/main/java/pangyo/makeat/controller/KakaoController.java
@@ -1,4 +1,4 @@
-package pangyo.makeat;
+package pangyo.makeat.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import pangyo.makeat.service.KakaoService;
 
 import java.io.IOException;
 import java.util.Map;
