6f145cb10a615a6045d4c82f4110e7e3ea88b37a
hanjaegyeong <hjg000223@naver.com>
2023-02-20 15:32:22 +0900

    feat: Users 컨트롤러 빠진 코드 추가

--- backend/src/main/java/pangyo/makeat/controller/UsersController.java
+++ backend/src/main/java/pangyo/makeat/controller/UsersController.java
@@ -38,6 +38,19 @@ public class UsersController {
     //Login
     @GetMapping("/login")
     public String loginPage(){
+        return "kakaoCI/login";
+    }
+
+    @GetMapping("/kakao")
+    public String getCI(@RequestParam String code, Model model) throws IOException, IOException {
+        System.out.println("code = " + code);
+        String access_token = ks.getToken(code);
+        Map<String, Object> userInfo = ks.getUserInfo(access_token);
+        model.addAttribute("code", code);
+        model.addAttribute("access_token", access_token);
+        model.addAttribute("userInfo", userInfo);
+
+        //ci는 비즈니스 전환후 검수신청 -> 허락받아야 수집 가능
         return "index";
     }
 
