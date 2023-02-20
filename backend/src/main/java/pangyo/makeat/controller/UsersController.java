745d4d918985f3ebe35b36db60b590e8b400a23d
hanjaegyeong <hjg000223@naver.com>
2023-02-20 15:26:23 +0900

    feat: Users 유저정보페이지 컨트롤러-서비스 기능 추가

--- backend/src/main/java/pangyo/makeat/controller/UsersController.java
+++ backend/src/main/java/pangyo/makeat/controller/UsersController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import pangyo.makeat.service.KakaoService;
+import pangyo.makeat.service.UserDataService;
 
 
 import java.io.IOException;
@@ -15,38 +16,34 @@ import java.util.Map;
 @Controller
 @RequestMapping("/users")
 public class UsersController {
+
     @Autowired
     KakaoService ks;
+    @Autowired
+    UserDataService us;
 
-    @GetMapping("/login")
-    public String loginPage()
-    {
-        return "kakaoCI/login";
+    //Users
+    @GetMapping("/info/{kakaoId}")
+    public String getUserData(@PathVariable String kakaoId) {
+        log.info("mappingPath userId={}", kakaoId);
+        return "userdata"; //유저 정보 페이지
     }
 
-    @GetMapping("/kakao")
-    public String getCI(@RequestParam String code, Model model) throws IOException, IOException {
-        System.out.println("code = " + code);
-        String access_token = ks.getToken(code);
-        Map<String, Object> userInfo = ks.getUserInfo(access_token);
-        model.addAttribute("code", code);
-        model.addAttribute("access_token", access_token);
-        model.addAttribute("userInfo", userInfo);
+    @PostMapping("/info/{kakaoId}")
+    public void postUserData(@PathVariable String kakaoId) throws IOException {
+        us.saveUserData(kakaoId);
+        log.info("mappingPath userId={}", kakaoId);
+    }
 
-        //ci는 비즈니스 전환후 검수신청 -> 허락받아야 수집 가능
+    //Login
+    @GetMapping("/login")
+    public String loginPage(){
         return "index";
     }
 
-    @GetMapping("/info/{userToken}")
-    public String getUserData(@PathVariable String userToken) {
-        log.info("mappingPath userId={}", userToken);
-        return "ok";
+    //QnA
+    @GetMapping("/info/qna")
+    public String getQna() {
+        return "qna";
     }
-
-    @PostMapping("/info/{userToken}")
-    public String postUserData(@PathVariable String userToken) {
-        log.info("mappingPath userId={}", userToken);
-        return "ok";
-    }
-
 }
