14b3956cf3bd025df6cca355deb4c1ccc82f9cbb
cookie0010 <sg.been2017@gmail.com>
2023-02-20 15:35:20 +0900

    임시 저장

--- backend/src/main/java/pangyo/makeat/controller/RecordController.java
+++ backend/src/main/java/pangyo/makeat/controller/RecordController.java
@@ -1,11 +1,15 @@
 package pangyo.makeat.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
 @RequestMapping("/record")
 public class RecordController {
+//    @GetMapping("/")
+
 }
