cd113364895670a25d8d626130d6c5c8d6d4dc30
cookie0010 <sg.been2017@gmail.com>
2023-02-20 14:50:18 +0900

    feat: ManyToOne fetch 수정

--- backend/src/main/java/pangyo/makeat/dto/Users.java
+++ backend/src/main/java/pangyo/makeat/dto/Users.java
@@ -14,7 +14,7 @@ public class Users {
     @Column(name="user_id")
     private Long userId;
     @Column(name="kakao_id")
-    private String kakaoId;
+    private Long kakaoId;
     @Column(name="user_token")
     private String userToken;
 }
--- backend/src/main/java/pangyo/makeat/service/UserService.java
+++ backend/src/main/java/pangyo/makeat/service/UserService.java
@@ -1,31 +1,30 @@
-package pangyo.makeat.service;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import pangyo.makeat.dto.Users;
-import pangyo.makeat.repository.UsersRepository;
-
-import java.util.List;
-
-@Service
-public class UserService {
-    @Autowired
-    private UsersRepository usersRepository;
-
-    /**
-     * 회원 가입 // 이게 필요할까?
-     */
-    public Long join(Users users) {
-        validateDuplicateMember(users); // 중복 회원 검증
-        usersRepository.save(users);
-
-        return users.getUserId();
-    }
-
-    private void validateDuplicateMember(Users users) {
-        List<Users> findUsers = usersRepository.findByName(users.getKakaoId());
-        if (!findUsers.isEmpty()) {
-            throw new IllegalStateException("이미 존재하는 회원입니다.");
-        }
-    }
-}
+//package pangyo.makeat.service;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//import pangyo.makeat.dto.Users;
+//
+//import java.util.List;
+//
+//@Service
+//public class UserService {
+//    @Autowired
+//    private UsersRepository usersRepository;
+//
+//    /**
+//     * 회원 가입 // 이게 필요할까?
+//     */
+//    public Long join(Users users) {
+//        validateDuplicateMember(users); // 중복 회원 검증
+//        usersRepository.save(users);
+//
+//        return users.getUserId();
+//    }
+//
+//    private void validateDuplicateMember(Users users) {
+//        List<Users> findUsers = usersRepository.findByName(users.getKakaoId());
+//        if (!findUsers.isEmpty()) {
+//            throw new IllegalStateException("이미 존재하는 회원입니다.");
+//        }
+//    }
+//}
